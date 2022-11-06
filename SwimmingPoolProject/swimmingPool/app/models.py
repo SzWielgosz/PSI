f018360b1daf3b8c3d3316f16b5541cb20e89bae
Szymon <szwielgosz259@gmail.com>
2022-11-06 13:05:35 +0100

    Adding class Ticket, ClientAdress, Client, TicketAssignment to models file

--- SwimmingPoolProject/swimmingPool/app/models.py
+++ SwimmingPoolProject/swimmingPool/app/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class WorkerAddress(models.Model):
     street = models.CharField(max_length=45, null=False)
-    houseNumber = models.IntegerField(3, null=False)
-    flatNumber = models.IntegerField(3)
-    postcode = models.IntegerField(45, null=False)
+    houseNumber = models.IntegerField(max_length=3, null=False)
+    flatNumber = models.IntegerField(max_length=3)
+    postcode = models.IntegerField(max_length=45, null=False)
     placeName = models.CharField(max_length=45, null=False)
 
 
@@ -13,17 +13,47 @@ class Worker(models.Model):
     name = models.CharField(max_length=45, null=False)
     surname = models.CharField(max_length=45, null=False)
     phoneNumber = models.IntegerField(max_length=9, null=False)
-    email = models.CharField(100)
-    pesel = models.IntegerField(9)
+    email = models.CharField(max_length=100)
+    pesel = models.IntegerField(max_length=11, null=False)
     workerAddress = models.ForeignKey(WorkerAddress, on_delete=models.CASCADE)
 
 
 class Shift(models.Model):
     startTime = models.DateTimeField(null=False)
     endTime = models.DateTimeField(null=False)
-    description = models.CharField(255)
+    description = models.CharField(max_length=255)
 
 
 class ShiftAssignment(models.Model):
     worker = models.ForeignKey(Worker, on_delete=models.CASCADE)
     shift = models.ForeignKey(Shift, on_delete=models.CASCADE)
+
+
+class Ticket(models.Model):
+    price = models.FloatField(null=False)
+    zone = models.CharField(max_length=45, null=False)
+    dateOfPurchase = models.DateTimeField(null=False)
+    dateOfEnd = models.DateTimeField(null=False)
+    worker = models.ForeignKey(Worker, on_delete=models.CASCADE)
+
+
+class ClientAdress(models.Model):
+    street = models.CharField(max_length=45, null=False)
+    houseNumber = models.IntegerField(max_length=3, null=False)
+    flatNumber = models.IntegerField(max_length=3)
+    postcode = models.CharField(max_length=11, null=False)
+    placeName = models.CharField(max_length=45, null=False)
+
+
+class Client(models.Model):
+    name = models.CharField(max_length=45, null=False)
+    surname = models.CharField(max_length=45, null=False)
+    phoneNumber = models.CharField(max_length=9, null=False)
+    email = models.CharField(max_length=100)
+    pesel = models.IntegerField(max_length=11, null=False)
+    adress = models.ForeignKey(ClientAdress, on_delete=models.CASCADE)
+
+
+class TicketAssignment(models.Model):
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
