6c989d88ecbbd09fa458830a56921dc47ec67e06
Adrian Sidor <>
2022-11-06 15:43:16 +0100

    adding app to instaled apps, mysql database and creating migrations

--- SwimmingPoolProject/swimmingPool/swimmingPool/settings.py
+++ SwimmingPoolProject/swimmingPool/swimmingPool/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
+    'app.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'swimmingPool.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'swimmingpool_database',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
